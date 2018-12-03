c1b330d5e16b495191dacc82e63c04c2948c2203
5ia17saott <5ia17saott@gibwintra.ch>
2018-12-03 16:15:10 +0100

    add calculator class with addtition method

---
+++ src/main/java/ch/bbw/consoleCalculator/Calculator.java
@@ -0,0 +1,12 @@
+package ch.bbw.consoleCalculator;
+
+public class Calculator {
+
+	private double nr1 = 0.0;
+	private double nr2 = 0.0;
+
+	public double calculateSum(double number1, double number2) {
+		return number1 + number2;
+	}
+
+}
